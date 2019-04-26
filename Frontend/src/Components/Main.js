4f1f8babdc2bff3eb8110296d3a2936239b4fc44
KavyaChennoju97 <47514924+KavyaChennoju97@users.noreply.github.com>
2019-04-26 08:20:56 -0700

    Update Main.js

--- Frontend/src/Components/Main.js
+++ Frontend/src/Components/Main.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Header from './Navigation/Header';
 import Home from './Home/Home';
+import QuestionAnswers from './Answers/QuestionAnswers';
+import AnswerForm from './Answers/AnswerForm';
 // import '../App.css';
 import SignUp from './Account/SignUp';
 import ChooseTopics from './Account/ChooseTopics';
 import SeeAllNotifications from './Notifications/SeeAllNotifications';
 import Profile from './Profile/Profile';
-import displayprofile from './Profile/displayprofile';
 
 class Main extends Component {
   render() {
-    
+    if ("jwtToken" in localStorage) {
       return (
         <div>
-          <Route path = "/header" component = {Header} />
-          <Route path = "/signup" component = {SignUp} />
-          <Route path = "/quora/topics" component = {ChooseTopics} />
-          <Route exact path="/home" component={Home}/>
-     <Route path = "/profile" component = {Profile} />
-        <Route exact path="/SeeAllNotifications" component={SeeAllNotifications}/>
-        <Route path = "/displayprofile" component = {displayprofile} />
-          <Route path = "/quora" component = {SignUp} />
+          <Route path="/header" component={Header} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/quora/topics" component={ChooseTopics} />
+          <Route path="/answer/question/:questionId" component={QuestionAnswers} />
+          <Route path="/answer/create" component={AnswerForm} />
+          <Route exact path="/quora/home" component={Home} />
+          <Route path="/profile" component={Profile} />
+         <Route path = "/displayprofile" component = {displayprofile} />
+          <Route exact path="/SeeAllNotifications" component={SeeAllNotifications} />
+        </div>
+     );
+    } else {
+      return (
+        <div>
+          <Route path="/quora" component={SignUp} />
         </div>
       )
-    
+    }
   }
 }
 
