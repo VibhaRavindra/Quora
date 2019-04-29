7d9e0532da1fef257e87831994310ade8ff25713
KavyaChennoju97 <47514924+KavyaChennoju97@users.noreply.github.com>
2019-04-29 16:29:15 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -44,7 +44,7 @@ app.use(session({
 
 //Allow Access Control
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://54.183.184.120:3000');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
@@ -66,7 +66,7 @@ var Search = require('./src/routes/Search')
 app.use(express.static('public'));
 
 //use cors to allow cross origin resource sharing
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://54.183.184.120:3000', credentials: true }));
 
 // parse application/x-www-form-urlencoded
 // for easier testing with Postman or plain HTML forms
