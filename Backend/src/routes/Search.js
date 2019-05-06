e87132646bb176fcf6b883aec713654441458606
Vibha Ravindra <vibhashree.ravindra@sjsu.edu>
2019-05-06 10:53:56 -0700

    added profile image to search profile pages and fixed bug in deactivate account

--- Backend/src/routes/Search.js
+++ Backend/src/routes/Search.js
@@ -3,10 +3,37 @@ var router = express.Router();
 var kafka = require('./kafka/client');
 const multer = require('multer');
 const FormData = multer();
+const userimage  =require('../models/DefaultUserImage');
 
 var jwt = require('jsonwebtoken');
 var passport = require('passport');
 var requireAuth = passport.authenticate('jwt', {session: false});
+
+router.get('/getRawImage',(req,res)=>{
+    console.log(req.query);
+    // yes make request to profile & reuse same code.
+    kafka.make_request('profile',{"path":"get_profile_pic", "body": req.query}, function(err,result){
+        if (err || result.status!== 200 || (result.status === 200 && result.base64[0].b64 === "default")) {
+            const image = Buffer.from(userimage.defaultImage, 'base64');
+            res.writeHead(200, {
+                'Content-Type': 'image/jpg',
+                'Content-Length': image.length
+            });
+            res.end(image)
+        }
+        else{
+            console.log(result);
+            var base64Data = result.base64[0].b64.replace(/^data:image\/jpg;base64,/, '');
+            const image = Buffer.from(base64Data, 'base64');
+            res.writeHead(200, {
+                'Content-Type': 'image/jpg',
+                'Content-Length': image.length
+            });
+            res.end(image)
+        }
+      });
+})
+
 
 router.get('/topics/:searchText', (req,res,next) => {
     let body = {
