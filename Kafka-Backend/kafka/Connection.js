72ca42a295380606f9c4c9b7ea2425e7e3ad6cc1
akhilasanka <akhila.sanka@sjsu.edu>
2019-05-05 23:31:03 -0700

    config for kafka consumer group

--- Kafka-Backend/kafka/Connection.js
+++ Kafka-Backend/kafka/Connection.js
@@ -1,10 +1,11 @@
 var kafka = require('kafka-node');
+let brokers = "18.144.17.51:2181,54.183.184.164:2181,18.144.47.158:2181";
 
 function ConnectionProvider() {
     this.getConsumer = function(topic_name) {
         //if (!this.kafkaConsumerConnection) {
-            this.client = new kafka.Client("localhost:2181");
-            this.kafkaConsumerConnection = new kafka.Consumer(this.client,[ { topic: topic_name, partition: 0}]);
+            this.client = new kafka.Client(brokers);
+            this.kafkaConsumerConnection = new kafka.Consumer(this.client,[ { topic: topic_name, partition: 0, groupId : "KafkaBackend"}]);
             this.client.on('ready', function () { console.log('Topic Consumer is ready for topic:', topic_name) })
         //}
         return this.kafkaConsumerConnection;
@@ -14,7 +15,7 @@ function ConnectionProvider() {
     this.getProducer = function() {
 
         if (!this.kafkaProducerConnection) {
-            this.client = new kafka.Client("localhost:2181");
+            this.client = new kafka.Client(brokers);
             var HighLevelProducer = kafka.HighLevelProducer;
             this.kafkaProducerConnection = new HighLevelProducer(this.client);
             console.log('Response Producer is ready!');
