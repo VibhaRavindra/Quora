3b0b1ade622606272a0cd15b688c663d5e972b8d
KavyaChennoju97 <kavya.chennoju@sjsu.edu>
2019-05-07 03:24:07 -0700

    added better functionality for follow on homepage

--- Frontend/src/Components/Question/DisplayQuestion.js
+++ Frontend/src/Components/Question/DisplayQuestion.js
@@ -16,6 +16,8 @@ class DisplayQuestion extends Component {
             followedquestions:[],
             openAnswer: '',
             follow:false,
+            status:[],
+            inc:[]
       
         }
     }
@@ -35,7 +37,12 @@ class DisplayQuestion extends Component {
 
     followquestion=(e,x,y)=>{
         console.log(localStorage.getItem("user_name"));
-        this.setState({follow:true})
+        var newstatus=this.state.status;
+        newstatus[x]=true;
+        console.log(newstatus,"eve new un")
+        var newinc=this.state.inc
+        newinc[x]=newinc[x]+1;
+        this.setState({status:newstatus,inc:newinc})
         swal("followed question");
         var data={
             follower_username:localStorage.getItem("user_name"),
@@ -43,24 +50,42 @@ class DisplayQuestion extends Component {
             question:y
         }
         axios.post("http://"+rooturl+":3001/quora/question/followquestion",data, localStorage.getItem('jwtToken'))
-        window.location.reload(true);
+    
     }
     unfollowquestion=(e,x,y)=>{
      console.log(localStorage.getItem("user_name"));
-     this.setState({follow:false})
-     console.log("hophop",x);
+     var newstatus=this.state.status;
+     newstatus[x]=false;
+     console.log(newstatus,"eve new un")
+     var newinc=this.state.inc
+     newinc[x]=newinc[x]-1;
+     this.setState({status:newstatus,inc:newinc})
      swal("unfollowed question");
-     this.setState({follow:false})
+    
      var data={
          follower_username:localStorage.getItem("user_name"),
          qid:x,
          question:y
      }
      axios.post("http://"+rooturl+":3001/quora/question/unfollowquestion",data, localStorage.getItem('jwtToken'))
-     window.location.reload(true);
+
  }
         
+componentDidMount(){
+    var status={},inc={};
 
+    this.props.question.followers.includes(localStorage.getItem("user_name")) ?
+    status[this.props.question._id]=true :  status[this.props.question._id]=false
+    if(this.props.question.followers)
+    inc[this.props.question._id]=this.props.question.followers.length
+       
+    this.setState({
+        status:status,inc:inc
+    });
+
+
+
+}
     render(){
 
       
@@ -68,25 +93,24 @@ class DisplayQuestion extends Component {
         let index = this.props.questionIndex;
         let answerDiv = null;
         let followDiv=null;
-        
+       
         if (record.answers.length>0) {
             let answer = record.answers[0];
             console.log(answer);
             answerDiv = <AnswerDetails answer={answer}/>;
         }
-        console.log(this.state.follow)
-        if(!record.followers.includes(localStorage.getItem("user_name")))
+        console.log(this.state.status[record._id])
+        if(this.state.status[record._id]===false)
         {
             followDiv=<div className="follow-icon answer-icon-label" onClick={e=>this.followquestion(e,record._id,record.question)}>
-            Follow {(record.followers.length == 0)? "": record.followers.length}</div>
+            Follow { this.state.inc[record._id]}</div>
   
         }
         else 
         {
             followDiv= <div id="unfollow-ques answer-icon-label" onClick={e=>this.unfollowquestion(e,record._id,record.question)}>
             <div className="home-follow-div home-follow-icon">
-            <img src={followImg} className="home-follow-img" width="60" height="40" alt="0"/>Unfollow {"  "}{(record.followers.length == 0)? ""
-            :record.followers.length}</div> </div>
+            <img src={followImg} className="home-follow-img" width="60" height="40" alt="0"/>Unfollow {"  "}{this.state.inc[record._id]}</div> </div>
               
         }
 
